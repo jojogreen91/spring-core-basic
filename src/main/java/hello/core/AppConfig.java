152e329014b3d5b38719dddb2d545d0ad6a3fd0d
jojogreen91 <iawbg13@gmail.com>
2021-10-05 11:00:23 +0900

    스프링 컨테이너에 스프링 빈 등록하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,27 +9,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 어플리케이션 동작에 필요한 객체의 생성과 연결을 담당하는 객체이다.
 // 동작 하는 객체들에게 의존성을 주입해주는 역할을 한다.
 // 객체를 실행하는 역할과 생성하고 연결하는 역할의 분리, 관심사 분리.
+@Configuration
 public class AppConfig {
 
     // 중복제거 리팩토링
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // 중복제거 리팩토링
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy(); // 새로운 할인 정책 적용
     }
 
+    @Bean
     public MemberService memberService () {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService () {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }

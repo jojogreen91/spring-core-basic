d14ff052479ff05482a54fd16f11e0197141f711
jojogreen91 <iawbg13@gmail.com>
2021-10-06 11:49:30 +0900

    웹 어플리케이션과 싱글톤 패턴

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,17 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    private SingletonService() {
+    }
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    public void logic() {
+        System.out.println("Singleton Logic Active.");
+    }
+}
